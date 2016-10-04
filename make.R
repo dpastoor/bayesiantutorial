852c78a1e0138ce810de41ca6de3c121f0fb9fe2
Devin Pastoor <devin.pastoor@gmail.com>
2016-10-04 13:24:40 -0400

    basic make script

---
+++ make.R
@@ -0,0 +1,16 @@
+### eventually render site here automatically
+## check at least lab-notebook and data exist, indicating reasonable
+## likelihood in the correct directory
+if (!all(dir.exists(c("lab-notebook", "data")))) {
+  stop("are you sure your working directory is the top level project
+       directory, it must be set there to render and move files correctly")
+}
+files_to_move <- list.files("lab-notebook/_book", full.names = T)
+
+dir.create("docs", recursive = T)
+copied <- file.copy(files_to_move, "doc", overwrite = T, recursive = T)
+
+if(!all(copied)) {
+  stop("could not copy all files, run each line of script manually to identify issue")
+}
+message("all files copied successfully")
