4b727ffed973526820b426524908e8469e3d3b48
Devin Pastoor <devin.pastoor@gmail.com>
2016-10-04 13:26:28 -0400

    clear out old docs before copying new

--- make.R
+++ make.R
@@ -7,8 +7,9 @@ if (!all(dir.exists(c("lab-notebook", "data")))) {
 }
 files_to_move <- list.files("lab-notebook/_book", full.names = T)
 
+unlink("docs", recursive = T, force = T)
 dir.create("docs", recursive = T)
-copied <- file.copy(files_to_move, "doc", overwrite = T, recursive = T)
+copied <- file.copy(files_to_move, "docs", overwrite = T, recursive = T)
 
 if(!all(copied)) {
   stop("could not copy all files, run each line of script manually to identify issue")
