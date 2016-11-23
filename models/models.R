5ca381561a86e039d7a6fd56ceca95a964ab310b
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-23 09:03:26 -0500

    control interactivity

--- models/models.R
+++ models/models.R
@@ -3,6 +3,14 @@ library(overseer)
 ## note, if this script is going to be run interactively, make sure to set the
 ## wd to the same location as the script itself
 ## setwd(file.path(getwd()," models"))
+
+if (interactive()) {
+  if (!file.exists("vanco_stockmann.cpp")) {
+    stop("make sure the directory is set to the models directory before running interactively,
+         to make sure the relative paths will be the same as when sourcing")
+  }
+}
+
 models <- Overseer$new()
 
 
