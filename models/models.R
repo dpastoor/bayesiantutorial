dc1540dfc998e44edf10aab8faabcaa3823b883b
Devin Pastoor <devin.pastoor@gmail.com>
2016-11-23 13:29:01 -0500

    interactive implementation actually doesn't work for sourcing files need to find another way

--- models/models.R
+++ models/models.R
@@ -3,16 +3,18 @@ library(overseer)
 ## note, if this script is going to be run interactively, make sure to set the
 ## wd to the same location as the script itself
 ## setwd(file.path(getwd()," models"))
-
-if (interactive()) {
-  if (!file.exists("vanco_stockmann.cpp")) {
-    stop("make sure the directory is set to the models directory before running interactively,
-         to make sure the relative paths will be the same as when sourcing")
-  }
-}
+# print(interactive())
+# print(getwd())
+# if (interactive()) {
+#   if (!file.exists("vanco_stockmann.cpp")) {
+#     stop("make sure the directory is set to the models directory before running interactively,
+#          to make sure the relative paths will be the same as when sourcing")
+#   }
+# }
 
 models <- Overseer$new()
 
 
 models$add_model_file("vanc_stockmann")
 
+models
